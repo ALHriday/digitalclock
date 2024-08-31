62ffe693e2aebe9e1c1a467e526ccf79ff388e7f
hriday <alauddinhriday@gmail.com>
2024-08-31 23:15:24 +0600

    Digital Clock

---
+++ index.js
@@ -0,0 +1,25 @@
+let hours = document.querySelector(".hours");
+let minutes = document.querySelector(".minutes");
+let seconds = document.querySelector(".seconds");
+let ampm = document.querySelector(".ampm");
+
+
+function updateTime() {
+
+    const time = new Date();
+    
+    let h = time.getHours();
+    let m = time.getMinutes();
+    let s = time.getSeconds();
+
+    h > 12 ? ampm.innerText = "PM" : "AM";
+
+    let hour = h > 12 ? h % 12 : h < 10 ? "0" + h : h;
+    let minute = m < 10 ? "0" + m : m;
+    let second = s < 10 ? "0" + s : s;
+
+    hours.innerText = hour;
+    minutes.innerText = minute;
+    seconds.innerText = second;  
+}
+setInterval(updateTime, 1000);
